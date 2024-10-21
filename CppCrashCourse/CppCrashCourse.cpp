15162c734014aa14f18594b274bcdea4bed06ff0
Jaimee Beard <jebeard@student.fullsail.edu>
2024-10-21 17:32:01 -0500

    Completed 1-1

--- CppCrashCourse/CppCrashCourse.cpp
+++ CppCrashCourse/CppCrashCourse.cpp
@@ -1,6 +1,16 @@
+#include <cstdio>
+
+// 1-1 
+int absolute_value(int x)
+{
+    if (x >= 0)
+        return x;
+    else
+        return -x;
+}
 
 int main(int argc, char* argv[])
 {
-    
-    return 0;
+    int my_num = -52;
+    printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
 }
