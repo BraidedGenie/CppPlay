4b1b7c30d434a37013843829045c2c69df79a239
Jaimee Beard <jebeard@student.fullsail.edu>
2024-10-21 18:02:13 -0500

    Completed 1-4

--- CppCrashCourse/CppCrashCourse.cpp
+++ CppCrashCourse/CppCrashCourse.cpp
@@ -9,8 +9,21 @@ int absolute_value(int x)
         return -x;
 }
 
+// 1-4
+int sum(int a, int b)
+{
+    return a + b;
+}
+
 int main(int argc, char* argv[])
 {
-    int my_num = -52;
-    printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+    // 1-1
+    // int my_num = -52;
+    // printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+
+    // 1-4
+    int num1 = 54;
+    int num2 = 35;
+
+    printf("The sum of %d and %d is %d.\n", num1, num2, sum(num1, num2));
 }
